591a37756d2d49ef24788a9c4094c725898f74ad
vFrez <vih.frez@mail.com>
2024-01-24 23:07:04 -0300

    Created profiles to spring

--- src/test/java/com/project/importer/service/PessoaServiceTest.java
+++ src/test/java/com/project/importer/service/PessoaServiceTest.java
@@ -9,10 +9,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PessoaServiceTest {
     @Autowired
     private PessoaService pessoaService;
